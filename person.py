4a1797566c041094f9e79719b158921e64f01c25
Sara Rincón <sararich@unisabana.edu.co>
2026-05-02 07:06:48 -0500

    Pregunta 5b

--- person.py
+++ person.py
@@ -1,1 +1,14 @@
-# person.py
+class Person:
+    def __init__(self, name):
+        self.name = name
+        self.accounts = []
+    
+    def add_account(self, account):
+        self.accounts.append(account)
+
+    def __str__(self):
+        numerocuentas = len(self.accounts)
+        return f"Name = {self.name}, Number of accounts = {numerocuentas}"
+
+
+        
