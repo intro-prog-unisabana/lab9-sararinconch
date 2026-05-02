7fa74774992df8e2748dcd8930c9a35509bd821f
Sara Rincón <sararich@unisabana.edu.co>
2026-05-02 09:22:07 -0500

    Problema 5d

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from utils import *
+from person import Person
+
 def main():
     people = []  # List to store all Person objects
 
@@ -13,15 +16,28 @@ def main():
 
         # Option 1: Add a new person
         if choice == "1":
-            pass
+            persona = person_data()
+            people.append(persona)
 
         # Option 2: Add an account to an existing person
         elif choice == "2":
-            pass
+            nombre = input("Enter the person's name:")
+            for persona in people:
+                if persona.name == nombre:
+                    cuenta = int(input("Enter a 4-digit account number:"))
+                    balance = float(input("Enter the initial balance:"))
+                    cuenta = BankAccount(cuenta, balance)
+                    persona.add_account(cuenta)
+                    break
+            else: 
+                print("Person not found.")
 
         # Option 3: Show all balances
         elif choice == "3":
-            pass
+            if len(people) == 0:
+                print("No data to show.")
+            else:
+                balance_summary(people)
 
         # Option 4: Quit
         elif choice == "4":
