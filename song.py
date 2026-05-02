06cddbeb83a4c26796af88cccc74ea49f27cd938
Sara Rincón <sararich@unisabana.edu.co>
2026-05-02 03:55:32 -0500

    Pregunta 1

--- song.py
+++ song.py
@@ -1,1 +1,10 @@
-# Write your code here!
+class Song:
+    def __init__(self, name, artist, length):
+        self.name = name
+        self.artist = artist
+        self.length = length
+
+    def get_length_in_seconds(self):
+        song_sec = self.length*60
+        return song_sec
+
