e2bbc1bef309cbaf6c28e659d94a179380a8fb16
Sara Rincón <sararich@unisabana.edu.co>
2026-05-02 06:08:43 -0500

    Pregunta 5a

--- bank_account.py
+++ bank_account.py
@@ -1,1 +1,21 @@
-# bank_account.py
+class BankAccount:
+    def __init__(self, account_number, balance = 0.0):
+        self.account_number = account_number
+        self.balance = balance
+    
+    def deposit(self, amount):
+        self.balance += amount
+    
+    def withdraw(self, amount):
+        if amount > self.balance:
+            return -1
+        else:
+            self.balance -= amount
+            return 0
+    
+    def __str__(self):
+        cuenta = str(self.account_number)
+        digits = cuenta[-2:]
+        return f"Account Number: **{digits}\nCurrent Balance: {self.balance}"
+
+    
